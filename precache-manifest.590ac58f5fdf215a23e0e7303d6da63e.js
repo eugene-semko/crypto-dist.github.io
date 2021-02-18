4ea0b09d21b8e4aab64fb95fedfe3e85408e1308
work-semko <work-semko@gmail.com>
2021-02-18 18:17:59 +0300

    root3

---
+++ precache-manifest.590ac58f5fdf215a23e0e7303d6da63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4350d10e19043f7ea5bb",
+    "url": "/css/app.94a42eaf.css"
+  },
+  {
+    "revision": "680bc4254ea572e59e6ea81638f68a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4350d10e19043f7ea5bb",
+    "url": "/js/app.0d539515.js"
+  },
+  {
+    "revision": "2c8420d846d566bd2abf",
+    "url": "/js/chunk-vendors.68ef552c.js"
+  },
+  {
+    "revision": "941a542086b12dbf61dc6b9837f7601e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
